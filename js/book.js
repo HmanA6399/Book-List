3bc840b02eeb03c5ef05089b5549ab88a831dc41
shaimaa aseem <shimaaaseem89@gmail.com>
2019-10-01 18:39:59 +0200

    editbook

--- js/book.js
+++ js/book.js
@@ -1,8 +1,9 @@
 // Book Class
 export default class Book {
   constructor(title, desc, author) {
-    this.title = title;
-    this.desc = desc;
-    this.author = author;
+     title;
+     desc;
+     author;
+     
   }
 }
